10648bc7d2a5e37073c546df8852c95f8bb61044
Slimecaster <nd070503@gmail.com>
2024-11-11 12:33:12 +0100

    Database forbindelse, og routes filen er klargjort

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const routes = require('./routes/routes')
+const mongoose = require("mongoose");
+
+try{
+    mongoose.createConnection('mongodb://localhost:27017/passuser', {connectTimeoutMS:10000})
+    console.log('MongoDB Connected');
+} catch(err){ console.error('MongoDB Connection Error', err)}
+
+try{
+    mongoose.createConnection('mongodb://localhost:27017/account', {connectTimeoutMS:10000})
+    console.log('MongoDB Connected');
+} catch(err){ console.error('MongoDB Connection Error', err)}
+
+
+app.use('/',routes)
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
 
---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const filmController = require('../controllers/controller');
+const model = require('../models/model');
+
+module.exports = router;
