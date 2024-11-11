ef4847107d0614f684533ab13c4be44ef2765115
Slimecaster <nd070503@gmail.com>
2024-11-11 12:40:54 +0100

    Database forbindelse, og routes filen er klargjort

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ try{
     console.log('MongoDB Connected');
 } catch(err){ console.error('MongoDB Connection Error', err)}
 
-
 app.use('/',routes)
+
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
