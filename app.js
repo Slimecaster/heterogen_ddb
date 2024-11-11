2890d16956fb127e7c685fd1dfc0efca643e25e6
amalieasborg <amalieasborg@gmail.com>
2024-11-11 12:43:26 +0100

    const path til middleware filsti

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const routes = require('./routes/routes')
 const mongoose = require("mongoose");
+const path=require('path');
 
 try{
     mongoose.createConnection('mongodb://localhost:27017/passuser', {connectTimeoutMS:10000})
