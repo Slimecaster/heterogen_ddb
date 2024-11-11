21d130e883006ace2d1a0e3f1a93ec10fd0093d0
amalieasborg <amalieasborg@gmail.com>
2024-11-11 12:32:55 +0100

    Account crud metoder

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,6 @@
 const Account=require('../models/model');
 
-
+//Tilføjer en ny account
 exports.createAccount = (req, res) => {
     try{
         const newAccount = new Account({
@@ -10,5 +10,42 @@ exports.createAccount = (req, res) => {
     }catch(err){
         console.error("Fejl ved tilføjelse af account:",err);
         res.status(500).send("Fejl ved tilføjelse af account.")
+    }
+};
+
+// Henter alle accounts og viser dem
+exports.getAllAccounts = async (req, res) => {
+    try {
+        const accounts = await Account.find();
+        res.render('index', { accounts });
+    } catch (err) {
+        console.error("Fejl ved hentning af account:", err);
+        res.status(500).send("Fejl ved hentning af account.");
+    }
+};
+
+// Opdaterer en account
+exports.updateAccount = async (req, res) => {
+    try {
+        await Account.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            email: req.body.email
+        });
+        res.redirect('/');
+    } catch (err) {
+        console.error("Fejl ved opdatering af account:", err);
+        res.status(500).send("Fejl ved opdatering af account.");
+    }
+};
+
+
+// Sletter en account
+exports.deleteAccount = async (req, res) => {
+    try {
+        await Account.findByIdAndDelete(req.params.id);
+        res.redirect('/');
+    } catch (err) {
+        console.error("Fejl ved sletning af account:", err);
+        res.status(500).send("Fejl ved sletning af account.");
     }
 };
