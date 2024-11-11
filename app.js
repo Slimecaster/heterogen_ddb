d13c87ecf771d299fde1af1ebeb94e2587560c40
Slimecaster <nd070503@gmail.com>
2024-11-11 13:16:22 +0100

    HTML (EJS)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes')
 const mongoose = require("mongoose");
 const path=require('path');
@@ -14,6 +15,9 @@ try{
     mongoose.createConnection('mongodb://localhost:27017/account', {connectTimeoutMS:10000})
     console.log('MongoDB Connected');
 } catch(err){ console.error('MongoDB Connection Error', err)}
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/',routes)
 
