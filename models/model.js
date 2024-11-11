342df8a0f623900f20e6a934dcb15883673c2c4f
Slimecaster <nd070503@gmail.com>
2024-11-11 11:22:55 +0100

    hi

--- models/model.js
+++ models/model.js
@@ -1,5 +1,17 @@
 const mongoose =require ('mongoose')
 
 const passuserSchema = new mongoose.Schema({
-
+    username: { type: String, required: true },
+    password: { type: String, required: true }
 })
+
+
+const accountSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true }
+})
+
+
+module.exports = mongoose.model('User', passuserSchema)
+module.exports = mongoose.model('Account', accountSchema)
